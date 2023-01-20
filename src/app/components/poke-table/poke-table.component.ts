c0c89e0cc2b4dc723969cfaba8fc10a6310db0dc
Daniel Socorro <daniel.socorro.c@gmail.com>
2023-01-20 12:09:55 -0300

    adding poke-table component

--- src/app/components/poke-table/poke-table.component.ts
+++ src/app/components/poke-table/poke-table.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 import { PokemonService } from 'src/app/services/pokemon.service'
 
 @Component({
@@ -8,6 +10,14 @@ import { PokemonService } from 'src/app/services/pokemon.service'
 })
 export class PokeTableComponent implements OnInit {
 
+  displayedColumns: string [] = ['position', 'image', 'name'];
+  data: any[] = [];
+  dataSource = new MatTableDataSource<any>(this.data);
+  pokemons = [];
+
+  @ViewChild(MatPaginator, { static: true })
+  paginator!: MatPaginator;
+
   constructor(private pokeService: PokemonService) {}
 
   ngOnInit(): void {
@@ -15,13 +25,35 @@ export class PokeTableComponent implements OnInit {
   }
 
   getPokemons() {
-    this.pokeService.getPokemons().subscribe(
-      res => {
-        console.log(res);
-      },
-      err => {
+    let pokemonData;
+    for(let i = 1; i <= 150; i++){
+      this.pokeService.getPokemons(i).subscribe(
+        res => {
+          pokemonData = {
+            position: i,
+            image: res.sprites.front_default,
+            name: res.name
+          };
+          this.data.push(pokemonData);
+          this.dataSource = new MatTableDataSource<any>(this.data);
+          this.dataSource.paginator = this.paginator;
+          console.log(res);
+        },
+        err => {
+          console.log(err);
+        }
+      );
 
-      }
-    )
+    }
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 }
+
